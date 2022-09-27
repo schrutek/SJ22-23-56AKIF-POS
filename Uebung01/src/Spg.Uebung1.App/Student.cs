59d045e7c327d1173d54f4271b629ff2cf8d68fb
schrutek <martin@schrutek>
2022-09-27 19:23:37 +0200

    Übung 1 gelöst

---
+++ Uebung01/src/Spg.Uebung1.App/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spg.Uebung1.App
+{
+    public class Student
+    {
+    }
+}
